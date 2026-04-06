3e7fb70b5132780afc05a4416676eb95082fca8f
Shahriar-Shakil-Khan <shahriarshakil28@gmail.com>
2026-04-06 13:16:17 +0600

    updated

--- genericFunction.ts
+++ genericFunction.ts
@@ -18,3 +18,36 @@ const arrString = createArrayWithGeneric("Shakil");
 const arrNumber = createArrayWithGeneric(42);
 const arrUserObj = createArrayWithGeneric({ id: 12345 , name: "Programmer" });
 
+
+const createArrayTupleWithGeneric = < X , y > ( value1 : X , value2 : y ) => [ value1 , value2 ]
+
+const res1 = createArrayTupleWithGeneric ("Shakil",45);
+const res2 = createArrayTupleWithGeneric ( { id: 12345 , name: "Programmer" } , [1,2,3] );
+
+
+
+const addStudentToCourse = <T>(studentInfo:T)=>{
+    return {
+
+        course: "Next Level",
+        ...studentInfo
+
+    };
+}
+
+const student2 = {
+    id:12345,
+    name:"Nabil",
+    age:25,
+    hasCar:false
+}
+
+const student1 = {
+    id:12345,
+    name:"Shakil",
+    age:30
+}
+
+
+const res3= addStudentToCourse(student2);
+console.log(res3);
