1db2771aaed21aba8a4ffee5d6cfc72faa2b6326
Shahriar-Shakil-Khan <shahriarshakil28@gmail.com>
2026-04-19 12:28:26 +0600

    update

---
+++ class.ts
@@ -0,0 +1,38 @@
+// 
+// class Animal {
+    
+//     name: string;
+//     species:string;
+//     sound:string;
+
+//     constructor(name: string , species:string , sound:string){
+
+//         this.name = name;
+//         this.species = species;
+//         this.sound = sound;
+//     }
+
+//     makeSound(){
+//         console.log(`${this.name} the ${this.species} says ${this.sound}`)
+//     }
+
+// }
+
+//Perameter properties
+
+class Animal {
+    
+    
+
+    constructor(public name: string , public species:string , public sound:string){
+    }
+
+    makeSound(){
+        console.log(`${this.name} the ${this.species} says ${this.sound}`)
+    }
+
+}
+
+const dog = new Animal("Bool Dog" ,"Dog", "Ghew Ghew")
+
+
