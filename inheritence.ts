01a495fb1b64929b8f7d3b5f72921994523c8eac
Shahriar-Shakil-Khan <shahriarshakil28@gmail.com>
2026-04-19 17:56:09 +0600

    updated

--- inheritence.ts
+++ inheritence.ts
@@ -1,19 +1,61 @@
-class Student{
+// class Student{
+//     name:string;
+//     address:string;
+//     age:number;
+
+//     constructor(name:string, address:string, age:number){
+//          this.name=name;
+//          this.address=address;
+//          this.age=age;
+//     }
+
+//     getInformation(numberOfHours:number){
+//         console.log(`I am ${this.name}. I am from ${this.address}. I am ${this.age} years old.I sleep for ${numberOfHours} hours.`)
+//     }
+// }
+
+// const student = new Student("Shakil","Dhaka",26) 
+
+// student.getInformation(8)
+
+
+class Parents{
     name:string;
     address:string;
     age:number;
 
     constructor(name:string, address:string, age:number){
-         this.name=name;
-         this.address=address;
-         this.age=age;
+        this.name=name;
+        this.address=address;
+        this.age=age;
+    }
+    getInformation(numberOfHours:number){
+        console.log(`I am ${this.name}. I am from ${this.address}. I am ${this.age} years old.I sleep for 
+        ${numberOfHours} hours.`)
     }
 
-    getInformation(numberOfHours:number){
-        console.log(`I am ${this.name}. I am from ${this.address}. I am ${this.age} years old.I sleep for ${numberOfHours} hours.`)
+} 
+
+class Student extends Parents{}
+
+class Teacher extends Parents{
+    designation:string;
+
+    constructor(name:string, address:string, age:number, designation:string){
+        super(name, address, age);
+        this.designation = designation;
+    }
+    getInformation1(numberOfHours:number){
+        console.log(`I am ${this.name}. I am from ${this.address}. I am ${this.age} years old.I sleep for 
+        ${numberOfHours} hours.i am a ${this.designation}.`)
     }
 }
 
-const student = new Student("Shakil","Dhaka",26) 
 
-student.getInformation(8)
+// For student
+const student = new Student("Shakil","Dhaka",26) 
+student.getInformation(9)
+
+//For Teacher
+const teacher = new Teacher("Nabil","Khulna",21,"pharmacist") 
+teacher.getInformation1(20)
