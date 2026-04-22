1f55ca9891dddc4e3ec87fea98187797949e9e38
Shahriar-Shakil-Khan <shahriarshakil28@gmail.com>
2026-04-22 11:07:03 +0600

    shakil

---
+++ instanceofguard.ts
@@ -0,0 +1,53 @@
+class Person{
+name : string;
+    constructor(name:string){
+    this.name = name;
+}
+
+getSleep(numOfHours: number){
+    console.log(`${this.name} is sleeping for ${numOfHours} hours.`);
+    }
+}
+
+class Student extends Person{
+
+    constructor(name:string){
+        super(name);
+    }
+
+    doStudy(numOfHours: number){
+        console.log(`${this.name} is studying for ${numOfHours} hours.`);
+    }
+
+}
+
+class Teacher extends Person{
+
+    constructor(name:string){
+        super(name);
+}
+
+    takeClass(numOfHours: number){
+    console.log(`${this.name} is taking a class for ${numOfHours} hours.`);
+}
+
+}
+
+
+const getUserInfo = (user : Person ) => {
+    if(user instanceof Student){
+        user.doStudy(10);
+    }else if(user instanceof Teacher){
+        user.takeClass(7);
+    }else{
+        user.getSleep(16)
+    }
+}
+
+const person1 = new Person("Mr. Sami");
+const student1 = new Student("Mr. Shakil");
+const teacher1 = new Teacher("Dr. Anika");
+
+getUserInfo(person1);
+getUserInfo(student1);
+getUserInfo(teacher1);
