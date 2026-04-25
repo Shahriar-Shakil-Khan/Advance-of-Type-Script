09f6c276e19f554b4453f42ac4dbcdd64d003531
Shahriar-Shakil-Khan <shahriarshakil28@gmail.com>
2026-04-25 13:06:21 +0600

    shakil

---
+++ allPractise.ts
@@ -0,0 +1,39 @@
+// const name : string = "shakil"
+// console.log(name)
+
+// const num : number = 10
+// console.log(num) 
+
+// const fraction: number = 30.56
+// console.log(fraction)
+
+// const nameArray: string[] = ["Shakil", "Rafi", "Mahadi"]
+// console.log(nameArray)
+
+// const nameArray: readonly string[] = ["Shakil", "Rafi", "Mahadi"]
+// console.log(nameArray)
+
+// const numArray: number[] = [100, 20, 30]
+// console.log(numArray)
+
+
+// numArray.push(5000)
+// console.log(numArray)
+
+// let ourTuple:[string, number, boolean];
+// ourTuple=["shakil",5000,false]
+// console.log(ourTuple)
+
+// let ourTuple :[string, number, boolean] = ["shakil", 5000, false];
+// console.log(ourTuple)
+
+
+
+
+
+
+
+
+
+
+
